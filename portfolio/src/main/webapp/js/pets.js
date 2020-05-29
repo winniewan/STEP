29031da0277443ef0d4691c5f95b5abf471ff618
Winnie <winniewan6@gmail.com>
2020-05-29 09:34:52 -0400

    Replace hardcoded number

--- portfolio/src/main/webapp/js/pets.js
+++ portfolio/src/main/webapp/js/pets.js
@@ -1,11 +1,11 @@
 function slideShow() {
     setTimeout(slideShow, 1000);
     const img = document.querySelectorAll(".image");
-    for(var i = 0; i < 7; i++) {
+    for(var i = 0; i < img.length; i++) {
         img[i].style.display = "none";
     }
 
-    const imgIndex = Math.floor(Math.random() * 7);
+    const imgIndex = Math.floor(Math.random() * img.length);
     img[imgIndex].style.display = "block";
 }
 
