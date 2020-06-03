5e1e513d36a4a92fcbd5f2843b550e85461f0e95
Winnie <winniewan6@gmail.com>
2020-06-03 14:26:59 +0000

    Remove empty line

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -2,7 +2,6 @@ package com.google.sps.data;
 
 /** An item on the comments section. */
 public final class Comment {
-  
   private final long id;
   private final String text;
   private final long timestamp;
