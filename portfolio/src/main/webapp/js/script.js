03860a3038e21e9da6e80ef111df47660d002494
Winnie <winniewan6@gmail.com>
2020-06-03 18:40:23 +0000

    Create javascript file to display comments

---
+++ portfolio/src/main/webapp/js/script.js
@@ -0,0 +1,25 @@
+function displayComments() {
+  const pageSize = document.getElementById('update-page').value;
+
+  fetch('/comments?page-size='+pageSize).then(response => response.json()).then((comments) => {
+    // comments is an object, not a string, so we have to
+    // reference its fields to create HTML content
+    const commentElement = document.getElementById('comment-section');
+    commentElement.innerHTML = '';
+    comments.forEach((comment) => {
+        commentElement.appendChild(createCommentElement(comment));
+    })
+  });
+}
+
+/** Creates an element that represents a comment. */
+function createCommentElement(comment) {
+  const commentElement = document.createElement('li');
+  commentElement.className = 'comment';
+
+  const textElement = document.createElement('span');
+  textElement.innerText = comment.text;
+
+  commentElement.appendChild(textElement);
+  return commentElement;
+}
