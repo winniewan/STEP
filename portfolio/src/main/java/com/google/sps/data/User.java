480fd0b768bc2142738140802afaeff810d03b00
Winnie <winniewan6@gmail.com>
2020-06-09 15:16:19 +0000

    Add documentation to constructor

--- portfolio/src/main/java/com/google/sps/data/User.java
+++ portfolio/src/main/java/com/google/sps/data/User.java
@@ -5,7 +5,14 @@ public final class User {
   private final boolean isLoggedin;
   private final String email;
   private final String url;
-
+  
+/**
+* User's information constructor.
+* 
+* @param  isLoggedin  boolean value whether user is logged in or not.
+* @param  email the user's email address.
+* @param  url the logged-in or logged-out URL links.
+*/
   public User(boolean isLoggedin, String email, String url) {
     this.isLoggedin = isLoggedin;
     this.email = email;
