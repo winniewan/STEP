dc3135158ba5ebb9dc9f7cd75c1b4f626d85ff4e
Winnie <winniewan6@gmail.com>
2020-06-09 20:50:17 +0000

    Add a map by using Maps API

---
+++ portfolio/src/main/webapp/js/maps.js
@@ -0,0 +1,6 @@
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
