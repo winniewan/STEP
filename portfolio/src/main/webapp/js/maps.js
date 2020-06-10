e029b2120364f15b66fbb1fb34870d1bee74b2c3
Winnie <winniewan6@gmail.com>
2020-06-10 14:29:28 +0000

    Add documentation

--- portfolio/src/main/webapp/js/maps.js
+++ portfolio/src/main/webapp/js/maps.js
@@ -1,5 +1,8 @@
 /** Creates a map and adds it to the page. */
 function createMap() {
+  // Map variable is assigned to a new Map object.
+  // Two required options for every map: center, zoom.
+  // This map centers at MTV, CA and zooms at street view.
   const map = new google.maps.Map(
       document.getElementById('map'),
       {center: {lat: 37.422, lng: -122.084}, zoom: 16});
