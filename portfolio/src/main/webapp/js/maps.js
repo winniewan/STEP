1c07ea9d6e7dc1d5ffb9947544e69d6e3017e707
Winnie <winniewan6@gmail.com>
2020-06-10 18:06:41 +0000

    Refactor maps javascript

--- portfolio/src/main/webapp/js/maps.js
+++ portfolio/src/main/webapp/js/maps.js
@@ -1,18 +1,19 @@
 /** Creates a map and adds it to the page. */
 function createMap() {
-  // NYC coordinates
-  nyc_lat = 40.6782;
-  nyc_lng = -73.9442;
-
-  // By default, this map centers at Brooklyn, NY.
-  const map = new google.maps.Map(
-      document.getElementById('map'),
-      {center: {lat: nyc_lat, lng: nyc_lng}, zoom: 10});
+  // NYC coordinates.
+  var nycLatLng = new google.maps.LatLng(40.6782, -73.9442);
+  var mapOptions = {
+    zoom: 10,
+    center: nycLatLng
+  }
   
-  // Marker for my hometown location: NYC.
-  const NYCMarker = new google.maps.Marker({
-    position: {lat: nyc_lat, lng: nyc_lng},
-    map: map,
-    title: 'Hometown: NYC'
+  var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: nycLatLng,
+    title:"Hometown!"
   });
+
+  // To add the marker to the map, call setMap();
+  marker.setMap(map);
 }
