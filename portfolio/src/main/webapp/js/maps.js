b0c2bbafa05681ecf2b8abe3219e3134a7202b37
Winnie <winniewan6@gmail.com>
2020-06-10 16:36:21 +0000

    Add marker and update project-id

--- portfolio/src/main/webapp/js/maps.js
+++ portfolio/src/main/webapp/js/maps.js
@@ -1,10 +1,14 @@
 /** Creates a map and adds it to the page. */
 function createMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+  // By default, this map centers at Brooklyn, NY.
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 40.6782, lng: -73.9442}, zoom: 10});
+  
+  // Marker for my hometown location: NYC.
+  const NYCMarker = new google.maps.Marker({
+    position: {lat: 40.6782, lng: -73.9442},
+    map: map,
+    title: 'Hometown: NYC'
+  });
 }
