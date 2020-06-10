3aae298dc1643788603291a872770b36b9c1d849
Winnie <winniewan6@gmail.com>
2020-06-10 16:58:07 +0000

    Create coord variables

--- portfolio/src/main/webapp/js/maps.js
+++ portfolio/src/main/webapp/js/maps.js
@@ -1,13 +1,17 @@
 /** Creates a map and adds it to the page. */
 function createMap() {
+  // NYC coordinates
+  nyc_lat = 40.6782;
+  nyc_lng = -73.9442;
+
   // By default, this map centers at Brooklyn, NY.
   const map = new google.maps.Map(
       document.getElementById('map'),
-      {center: {lat: 40.6782, lng: -73.9442}, zoom: 10});
+      {center: {lat: nyc_lat, lng: nyc_lng}, zoom: 10});
   
   // Marker for my hometown location: NYC.
   const NYCMarker = new google.maps.Marker({
-    position: {lat: 40.6782, lng: -73.9442},
+    position: {lat: nyc_lat, lng: nyc_lng},
     map: map,
     title: 'Hometown: NYC'
   });
