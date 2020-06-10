951361d5bf6b5dd90d8e5763b5572d016af6ad59
Winnie <winniewan6@gmail.com>
2020-06-10 15:07:44 +0000

    Remove unnecessary info

--- portfolio/src/main/webapp/js/maps.js
+++ portfolio/src/main/webapp/js/maps.js
@@ -1,7 +1,5 @@
 /** Creates a map and adds it to the page. */
 function createMap() {
-  // Map variable is assigned to a new Map object.
-  // Two required options for every map: center, zoom.
   // This map centers at MTV, CA and zooms at street view.
   const map = new google.maps.Map(
       document.getElementById('map'),
